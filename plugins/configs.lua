66f798cb2102fa1e33447cc0ac680281b109fc56
jlaneve <lanevejulian@gmail.com>
2022-06-25 14:49:36 -0500

    Fix nvim-cmp mappings
    See https://github.com/NvChad/NvChad/issues/1236

--- plugins/configs.lua
+++ plugins/configs.lua
@@ -1,9 +1,3 @@
-local present, cmp = pcall(require, "cmp")
-
-if not present then
-   return
-end
-
 local M = {}
 
 M.nvimtree = {
@@ -21,25 +15,29 @@ M.nvimtree = {
   },
 }
 
-M.cmp = {
-  mapping = {
-    ["<CR>"] = cmp.mapping.confirm {
-       behavior = cmp.ConfirmBehavior.Replace,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      fallback()
-    end, {
-      'i',
-      's',
-    }),
-  },
-  sources = {
-    { name = "luasnip", group_index = 2 },
-    { name = "nvim_lsp", group_index = 2 },
-    { name = "buffer", group_index = 2 },
-    { name = "nvim_lua", group_index = 2 },
-    { name = "path", group_index = 2 },
-  },
-}
+M.cmp = function()
+    local cmp = require "cmp"
+
+    return {
+      mapping = {
+        ["<CR>"] = cmp.mapping.confirm {
+           behavior = cmp.ConfirmBehavior.Replace,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          fallback()
+        end, {
+          'i',
+          's',
+        }),
+      },
+      sources = {
+        { name = "luasnip", group_index = 2 },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "buffer", group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path", group_index = 2 },
+      },
+    }
+end
 
 return M
